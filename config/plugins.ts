fe2bbca025d15426e8509661c57287a93963d259
Mekesia Brown <mekesiabrown@gmail.com>
2025-06-16 23:05:00 -0400

    tests: temporarily change bucket name; remove unneeded variable and console logs

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,18 +1,9 @@
 export default ({ env }) => {
   const region = env("AWS_REGION");
-  const bucket = env("AWS_BUCKET");
+  const bucket = "hello"; //env("AWS_BUCKET");
   const signedUrlExpires = parseInt(env("AWS_SIGNED_URL_EXPIRES")) || 900;
-  const baseUrl = `https://${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`;
   const accessKeyId = env("AWS_ACCESS_KEY_ID");
   const secretAccessKey = env("AWS_SECRET_ACCESS_KEY");
-
-  console.log("🧪 Render ENV: ", {
-    ACCESS_KEY_ID: env("AWS_ACCESS_KEY_ID"),
-    SECRET_ACCESS_KEY: env("AWS_SECRET_ACCESS_KEY") ? "it is set" : "not set",
-    BUCKET: env("AWS_BUCKET"),
-    REGION: env("AWS_REGION"),
-    SIGNED_URL_EXPIRES: env("AWS_SIGNED_URL_EXPIRES")
-  });
 
   return {
     upload: {

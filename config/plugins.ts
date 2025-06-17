0293e73e5f48e6fd05f3e9530b1b2c0ac0f411bc
Mekesia Brown <mekesiabrown@gmail.com>
2025-06-17 18:12:07 -0400

    refactor: refactor for AWS S3 signed url

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,12 @@
 export default ({ env }) => {
+  const acl = "private";
   const region = env("AWS_REGION");
   const bucket = env("AWS_BUCKET");
   const accessKeyId = env("AWS_ACCESS_KEY_ID");
   const secretAccessKey = env("AWS_SECRET_ACCESS_KEY");
   const signedUrlExpires = parseInt(env("AWS_SIGNED_URL_EXPIRES") || "900");
   const baseUrl = `https://${bucket}.s3.${region}.amazonaws.com`;
+  // const baseUrl = `https://s3.${region}.amazonaws.com/${bucket}`;
 
   console.log("✅ Strapi AWS S3 plugin configured with:");
   console.log(JSON.stringify({
@@ -23,29 +25,24 @@ export default ({ env }) => {
   return {
     upload: {
       config: {
-        provider: "aws-s3",
+        provider: 'aws-s3',
         providerOptions: {
-          baseUrl,
-          s3Options: {
-            credentials: {
-              accessKeyId,
-              secretAccessKey,
-            },
-            region,
-            params: {
-              Bucket: bucket,
-              ACL: "private",
-            },
+          credentials: {
+            accessKeyId,
+            secretAccessKey,
           },
-          signedUrlExpires,
+          region,
+          // baseUrl,
+          params: {
+            acl,
+            signedUrlExpires,
+            Bucket: bucket,
+          },
         },
         actionOptions: {
           upload: {},
           uploadStream: {},
           delete: {},
-          generateSignedUrl: {
-            expiresIn: signedUrlExpires,
-          },
         },
       },
     },
